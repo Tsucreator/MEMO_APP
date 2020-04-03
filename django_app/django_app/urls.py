932da41202d877c295c635ce4ba6fbb76dc62cb9
高井　真之介 <nosuke.liberty@gmail.com>
2020-04-04 04:07:07 +0900

    add logout button,some CSS

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.decorators import login_required
+from decorator_include import decorator_include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('memo_app.urls')),
+    path('', decorator_include(login_required, include('memo_app.urls'))),
     path('accounts/', include('allauth.urls')),
 ]
